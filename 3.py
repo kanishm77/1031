1fe3a42458fab512ffd84172ef1378806bd8da9b
kanishm77 <kanishm77@gmailcom>
2023-11-09 14:33:13 -0700

    chnages in calculator

--- 3.py
+++ 3.py
@@ -1,32 +1,41 @@
-import math
-a = int (input("enter a number"))
-b = int (input("enter another number "))
-operation = input("enter the intended operation(+,-,*,/)")
+# making a welcome message and asking the user to input some operation and desired numbers 
+print("Welcome to simple calculator.")
+print("I will add/subtract/multiple/divide any two numbers you provide.")
+first_number = input("Enter in first number: ")
+second_number = input("Enter in second number: ")
+operation = input("Would you like to add/subtract/multiple/divide: ")
 
-if(operation == "+"):
-    def add(a, b):
-       result = a + b
-       return result
-    sub_result = add(a,b)
-    print(f"The sum is: {sub_result}")
+# defining diffrent function according to the operation to be performed 
+def add(a, b):
+    return a + b
 
-elif(operation == "-"):
-    def sub(a, b):
-       result = a - b
-       return result
-    sub_result = sub(a,b)
-    print(f"The sub is: {sub_result}")
+def subtract(a, b):
+    return a - b
 
-elif(operation == "*"):
-    def mul(a, b):
-       result = a * b
-       return result
-    sub_result = mul(a,b)
-    print(f"The multiplacation is: {sub_result}")
+def multiply(a, b):
+    return a * b
 
-elif(operation == "/"):
-    def divide(a, b):
-       result = a / b
-       return result
-    sub_result = divide(a,b)
-    print(f"The division is: {sub_result}")
+def divide(a, b):
+    if b == 0:
+        return "Cannot be divided by zero"
+    return a / b
+
+
+# converting string data type to float 
+first_number = float(first_number)
+second_number = float(second_number)
+
+if operation == "add":
+    result = first_number + second_number
+    print(f"Result: {result}")
+elif operation == "subtract":
+    result = first_number - second_number
+    print(f"Result: {result}")
+elif operation == "multiply":
+    result = first_number * second_number
+    print(f"Result: {result}")
+elif operation == "divide":
+    result = first_number / second_number
+    print(f"Result: {result}")
+else:
+    print("Sorry, I do not understand your request.")
